930466add2585289e8708732aee9ce7b38f36dfb
uvudataclub2022 <uvudataclub@gmail.com>
2022-10-05 19:29:25 -0600

    chase first comment

--- Projects Hub/Fall 2022 Club Project/Example/user_behavior.py
+++ Projects Hub/Fall 2022 Club Project/Example/user_behavior.py
@@ -7,6 +7,8 @@ Python libraries allow users to extend the abilities of the language compiler. F
 
 # import libraries
 
+# Chase's first comment
+
 from turtle import clear
 import pandas as pd
 import numpy as np
