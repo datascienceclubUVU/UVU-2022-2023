846fd590eda26567ee7d74a7640ec4e0a168b56b
Data Science Club at UVU <uvudataclub@gmail.com>
2023-02-03 17:31:48 +0000

    skim down fact table

--- models/marts/core/fact_playlist_tracks.sql
+++ models/marts/core/fact_playlist_tracks.sql
@@ -1,6 +1,6 @@
 WITH all_columns AS (
-    SELECT *
-    FROM spotify.master
+    SELECT playlist_uri, track_uri, album_uri, added_at, added_by_uri, owner_uri
+    FROM {{ source('core', 'master')}}
 )
 
 SELECT * FROM all_columns
