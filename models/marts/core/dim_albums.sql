795c44995267a2c9d972c501ba4e49eca169cbc8
Data Science Club at UVU <uvudataclub@gmail.com>
2023-02-09 23:24:59 +0000

    update lineage for data model

--- models/marts/core/dim_albums.sql
+++ models/marts/core/dim_albums.sql
@@ -5,8 +5,8 @@
 }}
 
 WITH album_count AS (
-    SELECT album_uri, COUNT(*) AS num_occurrences, SUM(duration_ms) AS album_duration_ms
-    FROM {{ source('core', 'master') }}
+    SELECT DISTINCT album_uri, COUNT(*) AS num_occurrences
+    FROM {{ source('core', 'fact_playlist_tracks') }}
     GROUP BY album_uri
 ),
 
@@ -14,8 +14,8 @@ audio_features AS (
     SELECT album_uri, AVG(danceability) AS avg_danceability, AVG(energy) AS avg_energy,
         AVG(loudness) AS avg_loudness, AVG(acousticness) AS avg_acousticness_probability,
         AVG(speechiness) AS avg_speechiness_probability, CEILING(AVG(key)) most_common_key,
-        CEILING(AVG(mode)) most_common_mode, AVG(valence) AS avg_valence, AVG(tempo) AS avg_tempo
-    FROM spotify.master
+        CEILING(AVG(mode)) most_common_mode, AVG(valence) AS avg_valence, AVG(tempo) AS avg_tempo, SUM(duration_ms) album_duration_ms
+    FROM {{ source('core', 'master') }}
     GROUP BY album_uri
 ),
 
