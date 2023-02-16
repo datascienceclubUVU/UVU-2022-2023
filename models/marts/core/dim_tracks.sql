268e47ac8ad56f90ab2cd504df1db9833598b303
Data Science Club at UVU <uvudataclub@gmail.com>
2023-02-16 01:28:16 +0000

    update dim_tracks model

--- models/marts/core/dim_tracks.sql
+++ models/marts/core/dim_tracks.sql
@@ -12,7 +12,7 @@ WITH track_count AS (
 
 final AS (
     SELECT DISTINCT tc.track_uri, track_name, artist1, artist2, artist3, artist4, artist5, album_name, 
-    track_album_album_type AS track_type, release_date, "isExplicit", duration_ms AS track_duration_ms, 
+    release_date, "isExplicit", duration_ms AS track_duration_ms, 
     track_popularity AS popularity_rating, track_preview_url, tc.num_occurrences, danceability, energy, 
     loudness, acousticness, speechiness, liveness, key, mode, valence, tempo, time_signature
     FROM {{ source('core', 'master') }} sm
