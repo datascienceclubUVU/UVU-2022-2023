b8b26371c5528ec76d694531127d618f950030bc
nerdburton <10838359@my.uvu.edu>
2023-03-28 17:05:49 -0600

    added ability to create and pull client_id and client_secret as environmental variables

--- app/pages/3_Create_a_New_Playlist.py
+++ app/pages/3_Create_a_New_Playlist.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os
 
 st.set_page_config(page_title="Create a Playlist", page_icon="📈")
 
@@ -7,3 +8,15 @@ st.sidebar.header("Create a Playlist")
 st.write(
     """This demo creates a Playlist"""
 )
+
+#Windows Environment
+#$Env:SPOTIPY_CLIENT_ID = ''
+#$Env:SPOTIPY_CLIENT_SECRET = ''
+#
+#linux/docker
+#export SPOTIPY_CLIENT_ID = ''
+#export SPOTIPY_CLIENT_SECRET = ''
+
+
+client_id = os.getenv('SPOTIPY_CLIENT_ID')
+client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
