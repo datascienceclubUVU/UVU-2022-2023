ce4f9be85dfe9058293c34ddaacc6db1ab22e4b3
nerdburton <10838359@my.uvu.edu>
2023-04-06 19:19:34 -0600

    Pull required attributes from Spotify for given playlist

--- app/pages/1_Import_a_Playlist.py
+++ app/pages/1_Import_a_Playlist.py
@@ -37,21 +37,38 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials, requests_tim
 
 # Idea of this pipeline is to allow a user to paste in the URL for their playlist and extract the playlist URI
 # pl = 'https://open.spotify.com/playlist/4xjKk78zyTBjWzGjZ0HfDk?si=239973d3db4d41ce&pt=03042c4e3c2399a6889c1b8573e04052'
-pl = st.text_input(label="Please enter playlist URL:",value='https://open.spotify.com/playlist/4xjKk78zyTBjWzGjZ0HfDk?si=a3f654b6db204ca6')
+# C&C - https://open.spotify.com/playlist/4xjKk78zyTBjWzGjZ0HfDk?si=a3f654b6db204ca6
+# Crabs - https://open.spotify.com/playlist/1ZAo2hHlDWcCNNobChKp0D?si=56958c737d694edb
+pl = st.text_input(label="Please enter playlist URL:",value='https://open.spotify.com/playlist/1ZAo2hHlDWcCNNobChKp0D?si=56958c737d694edb')
 x= re.split('\/|\?',pl)
 playlist_id = x[4]
 
 try:
     playlist = sp.playlist(playlist_id=playlist_id)
-    playlist_info = sp.playlist_tracks(playlist_id=playlist_id)['items']
-    playlist_img = sp.playlist_cover_image(playlist_id=playlist_id)
-    st.image(playlist_img[0]['url'])
-    # used to pull track info
-    # sp.audio_features
-    df = pd.json_normalize(playlist_info)
-    df = df.drop(columns=['track.available_markets'])
-    df.to_csv('df_out.csv')
-    df2 = pd.json_normalize(playlist)
-    df2.to_csv('df2_out.csv')
+    playlist_image = playlist['images'][0]['url']
+    playlist_name = playlist['name']
+    playlist_description = ['description']
+    tracks = []
+    for x in playlist['tracks']['items']:
+        track_dict = {}
+        track_dict['track_name'] = x['track']['name']
+        track_dict['artist_name'] = x['track']['artists'][0]['name']
+        track_dict['album_name'] = x['track']['album']['name']
+        track_dict['release_date'] = release_date = x['track']['album']['release_date']
+        track_dict['release_year'] = release_date.split('-')[0]
+        track_dict['track_popularity'] = x['track']['popularity']
+        track_dict['duration_ms'] = x['track']['duration_ms']
+        track_dict['is_explicit'] = x['track']['explicit']
+        track_dict['track_uri'] = x['track']['uri']
+        audio_features = sp.audio_features(track_dict['track_uri'])
+        track_dict['danceability'] = audio_features[0]['danceability']
+        track_dict['energy'] = audio_features[0]['energy']
+        track_dict['instrumentalness'] = audio_features[0]['instrumentalness']
+        track_dict['valence'] = audio_features[0]['valence']
+        track_dict['tempo'] = audio_features[0]['tempo']
+        tracks.append(track_dict)
+    print(tracks)
+    st.image(playlist_image)
+
 except SpotifyException:
     st.write("That playlist does not exist or is private. Try again.")
