daaf1ea0fe7ca3be3ea970f66bbafba48c98c60c
nerdburton <10838359@my.uvu.edu>
2023-04-11 17:44:14 -0600

    Optimized audio_features pull for longer playlists. Now pulls paginated results.

--- app/pages/1_Import_a_Playlist.py
+++ app/pages/1_Import_a_Playlist.py
@@ -5,6 +5,7 @@ from spotipy import SpotifyClientCredentials
 from spotipy import SpotifyOauthError, SpotifyStateError, SpotifyException
 import re
 import os
+import math
 
 st.set_page_config(page_title="Import Playlist", page_icon="📈")
 
@@ -38,8 +39,9 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials, requests_tim
 # Idea of this pipeline is to allow a user to paste in the URL for their playlist and extract the playlist URI
 # pl = 'https://open.spotify.com/playlist/4xjKk78zyTBjWzGjZ0HfDk?si=239973d3db4d41ce&pt=03042c4e3c2399a6889c1b8573e04052'
 # C&C - https://open.spotify.com/playlist/4xjKk78zyTBjWzGjZ0HfDk?si=a3f654b6db204ca6
+# T&S - https://open.spotify.com/playlist/44TAn9OTwVDnKNzGY2JxOV?si=f4055168e94a42c7
 # Crabs - https://open.spotify.com/playlist/1ZAo2hHlDWcCNNobChKp0D?si=56958c737d694edb
-pl = st.text_input(label="Please enter playlist URL:",value='https://open.spotify.com/playlist/4xjKk78zyTBjWzGjZ0HfDk?si=a3f654b6db204ca6')
+pl = st.text_input(label="Please enter playlist URL:")#,value='https://open.spotify.com/playlist/4xjKk78zyTBjWzGjZ0HfDk?si=a3f654b6db204ca6')
 x= re.split('\/|\?',pl)
 playlist_id = x[4]
 
@@ -49,10 +51,11 @@ try:
         results = sp.next(playlist['tracks'])
         playlist['tracks']['items'].extend(results['items'])
         playlist['tracks']['next'] = results['next']
-    playlist_image = playlist['images'][0]['url']
+    playlist_image = sp.playlist_cover_image(playlist_id=playlist_id)[0]['url']
     playlist_name = playlist['name']
     playlist_description = ['description']
     tracks = []
+    track_uris = []
     for x in playlist['tracks']['items']:
         track_dict = {}
         track_dict['track_name'] = x['track']['name']
@@ -64,14 +67,29 @@ try:
         track_dict['duration_ms'] = x['track']['duration_ms']
         track_dict['is_explicit'] = x['track']['explicit']
         track_dict['track_uri'] = x['track']['uri']
-        audio_features = sp.audio_features(track_dict['track_uri'])
-        track_dict['danceability'] = audio_features[0]['danceability']
-        track_dict['energy'] = audio_features[0]['energy']
-        track_dict['instrumentalness'] = audio_features[0]['instrumentalness']
-        track_dict['valence'] = audio_features[0]['valence']
-        track_dict['tempo'] = audio_features[0]['tempo']
         tracks.append(track_dict)
-    print(len(tracks))
+        track_uris.append(track_dict['track_uri'])
+    audio_features = []
+    calls = math.ceil(len(track_uris) / 100)
+    print(calls)
+    for x in range(calls):
+        if len(track_uris) > 100:
+            uri_sublist = track_uris[:100]
+            track_uris = track_uris[100:]
+            af_results = sp.audio_features(uri_sublist)
+            audio_features.extend(af_results)
+        else:
+            af_results = sp.audio_features(track_uris)
+            audio_features.extend(af_results)
+    for track, audio in zip(tracks, audio_features):
+        track['danceability'] = audio['danceability']
+        track['energy'] = audio['energy']
+        track['instrumentalness'] = audio['instrumentalness']
+        track['valence'] = audio['valence']
+        track['tempo'] = audio['tempo']
+    
+    print(tracks[0])
+
     st.image(playlist_image)
 
 except SpotifyException:
